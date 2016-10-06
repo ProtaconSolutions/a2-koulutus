eb093f24b1f45c42a1f7a9a2ce3c9d291c2d9c52
tarlepp <tarmo.leppanen@kase.fi>
2016-10-06 13:45:29 +0300

    Added first route definition.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import { TodoComponent } from './todo/todo.component';
-import { TodoService } from "./todo.service";
+import { TodoService } from './todo.service';
+
+const routes = [
+  {
+    path: 'todos',
+    component: TodosComponent
+  }
+];
+
+/**
+ path?: string;
+ pathMatch?: string;
+ component?: Type<any>;
+ redirectTo?: string;
+ outlet?: string;
+ canActivate?: any[];
+ canActivateChild?: any[];
+ canDeactivate?: any[];
+ canLoad?: any[];
+ data?: Data;
+ resolve?: ResolveData;
+ children?: Route[];
+ loadChildren?: LoadChildren;
+ */
 
 @NgModule({
   declarations: [
@@ -19,6 +43,7 @@ import { TodoService } from "./todo.service";
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(routes),
     MaterialModule.forRoot()
   ],
   providers: [
