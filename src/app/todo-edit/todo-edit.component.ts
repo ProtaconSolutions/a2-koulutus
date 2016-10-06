688ef8e34c7287543d24afda55810ff118f63af6
tarlepp <tarmo.leppanen@kase.fi>
2016-10-06 15:31:30 +0300

    Implemented error messages to todo-edit form.

--- src/app/todo-edit/todo-edit.component.ts
+++ src/app/todo-edit/todo-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {TodoService} from "../todo.service";
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todo-edit',
